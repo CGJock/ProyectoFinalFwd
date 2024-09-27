f1e1b8cdea7a41384296c245a90b47b6d81324fd
CGJock <cjocksanest@gmail.com>
2024-09-27 07:51:50 -0600

    adelante front end register

---
+++ ReactApp/src/services/fetch.js
@@ -0,0 +1,22 @@
+
+//servicio con metodo post para registrar usuarios
+const postRegister = async (apiUrl,datos) => {
+    try {
+          const response = await fetch(apiUrl, {
+           method: 'POST',
+           headers: {
+             'Content-Type': 'application/json'
+             },
+             body: JSON.stringify((datos)//los datos contiene el objeto con los input
+              )
+           });
+           const data = await response.json()
+          
+           alert("se agrego con exito");
+         } catch(error) {
+            alert("error");
+           } 
+           
+      }
+      export default postRegister;
+//###################################################################################################################################

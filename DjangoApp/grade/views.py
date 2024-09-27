59a396d3e17ea4fd8093423211e8313e79434c03
CGJock <cjocksanest@gmail.com>
2024-09-27 02:27:38 -0600

    creacion de modelo de grados estudiantes

---
+++ DjangoApp/grade/views.py
@@ -0,0 +1,8 @@
+from grade.models import GRADE
+from .serializers import GradeSerializer
+from rest_framework import viewsets
+
+# Create your views here.
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = GRADE.objects.all()
+    serializer_class = GradeSerializer

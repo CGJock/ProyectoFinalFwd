72bccf7eb8d66b09ee9095e325d7c5c2f9145fff
monicabarriosg <monicabarrios0750@gmail.com>
2024-10-02 15:16:15 -0600

    documentacion de view

--- DjangoApp/post/serializers.py
+++ DjangoApp/post/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
-from .models import Post, User,PostResponse
+from .models import Post, User, PostResponse
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
         
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
 class PostResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostResponse 
         fields = '__all__'
+        
+class PostSerializer(serializers.ModelSerializer):
+    responses = PostResponseSerializer(many=True, read_only=True)
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'title', 'description', 'image_url', 'comment_count']
+        read_only_fields = ['creation_date', 'modification_date', 'comment_count']
+        
+
--- DjangoApp/post/views.py
+++ DjangoApp/post/views.py
@@ -3,25 +3,28 @@ from .models import Post, User, PostResponse
 from .serializers import PostSerializer, UserSerializer, PostResponseSerializer
 from rest_framework.response import Response
 import requests
-
+# queryset = Consulta de todos los usuarios en la base de datos.
+# serializer_class= Clase del serializador utilizado para convertir el modelo User a JSON y viceversa
 class userViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
+    queryset = User.objects.all() 
+    serializer_class = UserSerializer 
+#esta viewset gestiona las publicaciones en la application
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    #args es la solicitud que va a contener los datos de la nueva publibacion
     def create(self, request, *args, **kwargs):
         data = request.data
         image_file = request.FILES.get('image')  
-
-        
+    
+        #upload_to_imgur(image_file): Subida de archivos de imagen al servicio de Imgur.
         if image_file:
             imgur_url = self.upload_to_imgur(image_file)
             if imgur_url:
                 
                 data['image_url'] = imgur_url
             else:
+                #response devuelve ya sea un error de la solicitud o el estado de la misma 
                 return Response({"error": "Error uploading image to Imgur"}, status=status.HTTP_400_BAD_REQUEST)
 
         
@@ -44,7 +47,6 @@ class PostViewSet(viewsets.ModelViewSet):
         response = requests.post('https://api.imgur.com/3/image', headers=headers, files=files)
 
         if response.status_code == 200:
-            # Retornar la URL de la imagen subida
             return response.json()['data']['link']
         else:
             return None

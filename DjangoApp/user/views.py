f7d399841e4b8e4c38774ceaa0576185f946d9b2
CGJock <cjocksanest@gmail.com>
2024-10-02 08:29:45 -0600

    commit del dia preventivo

--- DjangoApp/user/views.py
+++ DjangoApp/user/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from user.models import USERS
 from .serializers import UserSerializer,UserLoginSerializer
+import jwt, datetime
+
 
 
 class RegisterUserViewSet(viewsets.ModelViewSet):
@@ -37,8 +39,50 @@ class LoginUserViewSet(viewsets.ViewSet):
         if not user.check_password(password):
             raise AuthenticationFailed("Incorrect password")
         
-        return Response({"message": "ok"}, status=status.HTTP_200_OK)
+        # se setan las propiedades del payload
+        payload = {
+            "id_user": user.id_user,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=120),#se setea el vencimiento del token
+            "iat" : datetime.datetime.utcnow()
+        }
+        
+        #se encripta el token 
+        token =  jwt.encode(payload, "secret", algorithm="HS256")
+        
+        response = Response()
+        
+        response.set_cookie('jwt', value=token, httponly=True)#se setea la cookie
+        
+        response.data = {
+            'jwt': token
+        }
+        return response
+
+      
+        # return Response({'jwt': token}, status=status.HTTP_200_OK)
             
+            
+class UserViewSet(viewsets.ModelViewSet):
+   queryset =  USERS.objects.all()
+   serializer_class = UserLoginSerializer
+   
+   def create(self,request):
+        token = request.COOKIES.get('jwt')
+       
+        if not token:
+           raise AuthenticationFailed("no autentificado")
+        try:
+           token = jwt.decode(token, 'secret', algorithm='HS256')
+           
+        except jwt.ExpiredSignatureError :
+            raise AuthenticationFailed("no autentificado")
+        
+        user =  USERS.objects.filter(id_user=token['id_user']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+
 
         
         

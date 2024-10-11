35e10e30c31b212c327d2c46f4fd81b1e9b916f5
CGJock <cjocksanest@gmail.com>
2024-10-11 09:42:18 -0600

    reestructuracion de carpetas

--- ReactApp/src/routes/routes.jsx
+++ ReactApp/src/routes/routes.jsx
@@ -8,17 +8,17 @@ import Home from '../pages/home/home'
 import Administration from '../pages/admin/administration';
 import Register from '../components/Admin-components/admin-ui/Register';
 import FAQ from '../components/home-components/FAQ';
-import PostList from '../components/Admin-components/post/PostList';
+import PostList from '../components/post/PostList';
 // import CreatePost from '../components/Admin-components/post/CreatePost';
 import Login from '../pages/login/login'
 // import PostForm from '../components/Admin-components/post/PostForm';
-import UsersContainer from '../components/administration-components/UsersContainer';
+import UsersContainer from '../components/Admin-components/admin-ui/UsersContainer';
 import { Profile } from '../pages/profile/Profile';
 import { useParams } from 'react-router-dom';
-import { ProfilePsychologist } from '../pages/psychologist/Profile-psychologist';
+import { ProfilePsychologist } from '../pages/psychologist/psychologist';
 
 
-import ProfilePsycho from "../pages/psychogist/PhofilePsycho";
+import ProfilePsycho from "../components/psychologist-components/PhofilePsycho";
 import AboutMe from '../components/home-components/AboutMe';
 import NavHome from '../components/home-components/NavHome';
 
